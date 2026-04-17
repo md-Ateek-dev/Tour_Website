93f4ec3be64bc8c4bb7fd74ab4456fb93e1e62c2
md-Ateek-dev <mohdateek.dev@gmail.com>
2026-04-17 11:59:05 +0530

    change file name

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import Breadcrumb from '../components/breadcrumb/Breadcrumb';
-import ContactSection from '../components/contactUs/ContactSection';
+import ContactSection from '../components/ContactUs/ContactSection';
 
 const ContactUs = () => {
     return (
